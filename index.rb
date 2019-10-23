e68bc6514711cb59e3690bb9eca9e20139c6fd1a
Edemone <edem.agbenyo@gmail.com>
2019-10-23 16:06:36 +0000

    Fix error to pass stickler

--- index.rb
+++ index.rb
@@ -23,7 +23,9 @@ def bubble_sort_by(arr)
     n = 0
     while n < (arr.size - 1 - i)
       count += 1
-      arr[n], arr[n + 1] = arr[n + 1], arr[n] if yield(arr[n], arr[n + 1]).positive? if block_given?
+      if block_given?
+        arr[n], arr[n + 1] = arr[n + 1], arr[n] if yield(arr[n], arr[n + 1]).positive?
+      end
       n += 1
     end
   end
