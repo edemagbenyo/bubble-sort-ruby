dc5d0ad21e2da9eddd248ed9acd7c48f1e914de2
Edemone <edem.agbenyo@gmail.com>
2019-10-23 15:59:38 +0000

    Fix error to pass stickler

--- index.rb
+++ index.rb
@@ -1,21 +1,20 @@
-#Bubble sort function
+# frozen_string_literal: true
 
 def bubble_sort(arr)
-
   k = arr.size
   count = 0
   k.times do |i|
     n = 0
     while n < (arr.size - 1 - i)
       count += 1
-      arr[n], arr[n+1] = arr[n+1], arr[n] if arr[n] > arr[n+1]
+      arr[n], arr[n + 1] = arr[n + 1], arr[n] if arr[n] > arr[n + 1]
       n += 1
     end
   end
   arr
 end
 
-bubble_sort([9,7,6,5,4,3,2,1])
+bubble_sort([9, 7, 6, 5, 4, 3, 2, 1])
 
 def bubble_sort_by(arr)
   k = arr.size
@@ -24,15 +23,11 @@ def bubble_sort_by(arr)
     n = 0
     while n < (arr.size - 1 - i)
       count += 1
-      if block_given?
-        if yield(arr[n], arr[n+1]).positive?
-          arr[n], arr[n+1] = arr[n+1], arr[n] 
-        end 
-      end
+      arr[n], arr[n + 1] = arr[n + 1], arr[n]  if yield(arr[n], arr[n + 1]).positive? if block_given?
       n += 1
     end
   end
   arr
 end
 
-bubble_sort_by(%w{hi hello hey}){ |x, y| x.length - y.length }
+bubble_sort_by(%w[hi hello hey]){ |x, y| x.length - y.length }
