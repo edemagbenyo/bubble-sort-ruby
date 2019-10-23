1610acae7016375d5529b9adca159e1434175de5
Edemone <edem.agbenyo@gmail.com>
2019-10-23 15:55:20 +0000

    Fix error to pass stickler

--- index.rb
+++ index.rb
@@ -1,22 +1,15 @@
+#Bubble sort function
+
 def bubble_sort(arr)
-    
-=begin
-Loop through the array
-    check element at index i and i+1
-    if arr[i] > arr[i+1] then we swap
-    else we do nothing
-=end
+
   k = arr.size
   count = 0
   k.times do |i|
     n = 0
-    while n < (arr.size - 1 -i)
-      count +=1
-      if arr[n] > arr[n+1]
-        arr[n],arr[n+1] = arr[n+1], arr[n] 
-      end
-      n+=1
-        
+    while n < (arr.size - 1 - i)
+      count += 1
+      arr[n], arr[n+1] = arr[n+1], arr[n] if arr[n] > arr[n+1]
+      n += 1
     end
   end
   arr
@@ -29,17 +22,17 @@ def bubble_sort_by(arr)
   count = 0
   k.times do |i|
     n = 0
-    while n < (arr.size - 1 -i)
-      count +=1
+    while n < (arr.size - 1 - i)
+      count += 1
       if block_given?
-        if yield(arr[n],arr[n+1]).positive?
-          arr[n],arr[n+1] = arr[n+1], arr[n] 
+        if yield(arr[n], arr[n+1]).positive?
+          arr[n], arr[n+1] = arr[n+1], arr[n] 
         end 
       end
-      n+=1
+      n += 1
     end
   end
   arr
 end
 
-bubble_sort_by(["hi","hello","hfffffey", "hello"]){|x,y|x.length - y.length}
+bubble_sort_by(%w{hi hello hey}){ |x, y| x.length - y.length }
