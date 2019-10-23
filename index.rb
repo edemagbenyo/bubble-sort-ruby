4378deacd7dd843bd02c782817fec9e79177c480
John Kilonzi <mrjohnkilonzi@gmail.com>
2019-10-23 18:37:27 +0300

    Added the bubble sort by method, tested with/out block

--- index.rb
+++ index.rb
@@ -6,7 +6,6 @@ Loop through the array
     if arr[i] > arr[i+1] then we swap
     else we do nothing
 =end
-
   k = arr.size
   count = 0
   k.times do |i|
@@ -20,6 +19,27 @@ Loop through the array
         
     end
   end
+  arr
 end
 
 bubble_sort([9,7,6,5,4,3,2,1])
+
+def bubble_sort_by(arr)
+  k = arr.size
+  count = 0
+  k.times do |i|
+    n = 0
+    while n < (arr.size - 1 -i)
+        count +=1
+        if block_given?
+          if yield(arr[n],arr[n+1]).positive?
+              arr[n],arr[n+1] = arr[n+1], arr[n] 
+          end 
+        end
+        n+=1
+    end
+  end
+  arr
+end
+
+bubble_sort_by(["hi","hello","hfffffey", "hello"]){|x,y|x.length - y.length}
