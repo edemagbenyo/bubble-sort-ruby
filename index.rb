71ff6d8a8e8437daf9e6bf2e118560c3b0760ddc
Edemone <edem.agbenyo@gmail.com>
2019-10-24 06:19:11 +0000

    Change position of block_given

--- index.rb
+++ index.rb
@@ -19,12 +19,12 @@ bubble_sort([9, 7, 6, 5, 4, 3, 2, 1])
 def bubble_sort_by(arr)
   k = arr.size
   count = 0
+  raise 'No block given. Please supply a block.' unless block_given?
+  
   k.times do |i|
     n = 0
     while n < (arr.size - 1 - i)
       count += 1
-      raise 'No block given. Please supply a block.' unless block_given?
-
       arr[n], arr[n + 1] = arr[n + 1], arr[n] if yield(arr[n], arr[n + 1]).positive?
       n += 1
     end
